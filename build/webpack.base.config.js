533ae5ad0017839014dc5bb16c45e44b755a7f72
Xotic750 <xotic750@gmail.com>
2018-01-24 21:50:16 +0100

    Reslove other extensions

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -209,7 +209,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.vue'],
+        extensions: ['.js', '.jsx', '.json', '.vue'],
         alias: {
             vue$: 'vue/dist/vue.esm.js',
             '@': resolve('src'),
